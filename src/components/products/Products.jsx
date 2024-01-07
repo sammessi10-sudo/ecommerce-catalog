d72107d9acdd70f7b7612a933caebe34d92863e4
sammy <samirmahapatra16@gmail.com>
2024-01-07 22:00:04 +0530

    initial_commit

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -3,15 +3,15 @@ import { useQuery } from '@tanstack/react-query'
 import axios, { all } from 'axios';
 
 const Products = () => {
-    const fetchProducts = async() => {
-        const url = 'https://dummyjson.com/products';
+    const fetchProducts = async() => {                  // Function to fetch all the products through axios.
+        const url = 'https://dummyjson.com/products';  
         const res = await axios.get(url)
         const data = await res.data;
         
         return data;
     }
     
-    const {data: allProducts, isLoading, isError, isSuccess} = useQuery({
+    const {data: allProducts, isLoading, isError, isSuccess} = useQuery({      // Fetching all the products through useQuery.
         queryFn: fetchProducts,
         queryKey: ['products']
     })
@@ -19,11 +19,11 @@ const Products = () => {
 
     return (
     <div className='h-full w-full grid grid-cols-5 p-2 overflow-scroll gap-4'>
-        {isSuccess && allProducts.products.map(product => 
+        {isSuccess && allProducts.products.map(product =>       // Checking if the data is fetched, then mapping the data to individual products. 
 
         <div key={product.id} className='flex flex-col bg-zinc-950'>
             <div >
-                {<img src={product.images[0]} className='object-contain h-44 w-full rounded-sm border-1px-solid'/>}
+                {<img src={product.images[0]} className='object-contain h-44 w-full rounded-sm border-1px-solid'/>} 
             </div>
             <div>
                 <h1 className='bg-red-500'>{product.title}</h1>
